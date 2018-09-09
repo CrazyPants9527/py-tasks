b99dc446b8a88ddc410d82e17532f6195f872646
CrazyPants9527 <291366458@Qqq.com>
2018-09-10 02:11:29 +0800

    weather forecast

--- Weather/weather.py
+++ Weather/weather.py
@@ -5,7 +5,7 @@ import argparse
 #显示当天天气预报
 def showmsg(weather_data):
     #显示时间
-    date = weather_data['result']['realtime']
+    date = weather_data['result'].get('realtime')
     print('地点:{0} 现在时间：{1} 农历：{2} {3}'.format(date['city_name'], date['date'],
                                               date['moon'], date['time']))
     #预报天气状况
@@ -37,16 +37,16 @@ def showmsg(weather_data):
 def future_weather(weather_data):
     weather = weather_data['result']['weather']
     for i in range(1, len(weather)):
-        print(weather[i]['date'])  #未来的时间
+        print("未来第{0}天，日期是{1}，天气预计如下：".format(i, weather[i]['date']))  #未来的时间
         weinfo = weather[i]['info']
         for k, v in weinfo.items():
             print(k, ':', v)
 
 
 #接收一个城市名，返回api对该城市的所有天气预报信息
-def weather_info(city):
-    appkey = '2bcac5f5ad4e497eade7e600898e13f3'
-    url = 'http://api.avatardata.cn/Weather/Query'
+def weather_info(city,
+                 appkey='2bcac5f5ad4e497eade7e600898e13f3',
+                 url='http://api.avatardata.cn/Weather/Query'):
     p = {'key': appkey, 'cityname': city}
     s = requests.get(url, params=p)
     weather_data = s.json()
@@ -54,12 +54,16 @@ def weather_info(city):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("city", type=str, help="输入一个城市名")
-    parser.add_argument("-f", "--future", help="输入-f,显示该城市未来四天天气预报")
-    args = parser.parse_args()
-    weather_data = weather_info(args.city)
-    if args.future:
-        future_weather(weather_data)
-    else:
-        showmsg(weather_data)
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("city", type=str, help="输入一个城市名")
+        parser.add_argument(
+            "-f", "--future", action="store_true", help="输入-f,显示该城市未来四天天气预报")
+        args = parser.parse_args()
+        weather_data = weather_info(args.city)
+        if args.future:
+            future_weather(weather_data)
+        else:
+            showmsg(weather_data)
+    except Exception as e:
+        print("你输入的城市名不存在，请再来一次")
